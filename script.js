ffe50f889db58b13dc41a839ac551d7dc5b8b054
NikhilJagtap1806 <jagtapnikhil1806@gmail.com>
2024-02-08 00:38:32 +0530

    portfolio Completed

--- script.js
+++ script.js
@@ -26,3 +26,50 @@ function showPopup(bool) {
     document.getElementById('popup').style.visibility = 'hidden'
   }
 }
+
+
+
+
+
+  // Function to handle the download process
+  // function downloadCV() {
+  //   // Create a new jsPDF instance
+  //   const doc = new jsPDF();
+
+  //   // Get HTML content to be converted to PDF
+  //   const htmlContent = document.querySelector('html');
+
+  //   // Options for the PDF generation (optional)
+  //   const options = {
+  //     background: 'white',
+  //     scale: 3 // Adjust scale as needed
+  //   };
+
+  //   // Convert HTML to PDF
+  //   doc.html(htmlContent, options).then(() => {
+  //     // Save the PDF with a filename
+  //     doc.save('Jagtap_Nikhil.pdf');
+  //   });
+  // }
+
+  // // Add event listener to the download button
+  // document.getElementById('downloadCVBtn').addEventListener('click', downloadCV);
+
+
+  
+  
+  
+  
+    document.getElementById('downloadBtn').addEventListener('click', function() {
+      // Replace "path/to/your/pdf/file.pdf" with the actual path to your PDF file on the server
+      const pdfFilePath = './Jagtap_Nikhil.pdf';
+  
+      // Use anchor element to trigger download
+      const link = document.createElement('a');
+      link.href = pdfFilePath;
+      link.download = 'Jagtap_Nikhil.pdf'; // Replace with the desired filename for the downloaded file
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  
